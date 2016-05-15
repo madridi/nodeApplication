50c2ca742449fc2d9e0580a988448736082a271a
dali <madridi@linagora.com>
2016-05-15 13:05:59 +0100

    mongodb connection

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 var _ = require('underscore');
+var mongodb = require('mongodb');
+var uri = 'mongodb://localhost:27017/example';
+mongodb.MongoClient.connect(uri, function(error, db) {
+  if(error) {
+    console.log("failed to connect");
+    process.exit(1);
+  }
+  db.collection('sample').insert({name: "DALI",country: "tunis"}, function(error, result) {
+    if (error) {
+      console.log("failed to insert data");
+      process.exit(1);
+    }
+    db.collection('sample').find().toArray(function(error, docs) {
+      console.log(docs);
+      process.exit(1);
+    });
+  });
+}); 
+
 _.each([1,2,3], function(v) {
   console.log(v);
 }) 
